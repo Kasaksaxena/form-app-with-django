b9d464f14704ffcb8b888232721e72c2fe573b30
Kasaksaxena <kasaksaxena6@gmail.com>
2025-01-13 21:26:22 +0530

    customizing admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Form
 
+class FormAdmin(admin.ModelAdmin):
+    list_display=("first_name","last_name","email","date","occupation")
+    search_fields=("first_name","last_name","email","date","occupation")
+    list_filter=("occupation","date")
+    ordering=("first_name", )
+    readonly_fields=("occupation",)
 # Register your models here.
-admin.site.register(Form)
+admin.site.register(Form,FormAdmin)
+#username:kasak
